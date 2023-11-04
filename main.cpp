2cbf69e32aaa3d5d240380114d33fdb614cef727
AdomasKairys <adomas.kairys@ktu.edu>
2023-11-04 23:48:45 +0200

    working on ld2

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <mpi.h>
-
+const int ROOT_PROCCESS = 0;
+const int DATA_PROCCESS = 1;
+const int RESULT_PROCCESS = 2;
 int main(){
     MPI::Init();
     auto rank = MPI::COMM_WORLD.Get_rank();
     auto totalProcesses = MPI::COMM_WORLD.Get_size();
-    if(rank == 0){
-        std::cout << "Proccess count " << totalProcesses << std::endl;
-        char name[MPI::MAX_PROCESSOR_NAME];
-        int name_length = 0;
-        MPI::Get_processor_name(name, name_length);
-        std::cout << "Processor name " << name << std::endl;
-        int sent_message = 0, received_message = 0;
-        MPI::COMM_WORLD.Send(&sent_message, 1, MPI::INT, 1, 1);
-        std::cout << "Sent message " << sent_message << std::endl;
-        MPI::COMM_WORLD.Recv(&received_message, 1, MPI::INT, 1, 1);
-        std::cout << "Received message " << received_message << std::endl;
-    } else {
-        int sent_message = 1, received_message = 1;
-        MPI::COMM_WORLD.Recv(&received_message, 1, MPI::INT, 0, 1);
-        std::cout << "Received message " << received_message << std::endl;
-        MPI::COMM_WORLD.Send(&sent_message, 1, MPI::INT, 0, 1);
-        std::cout << "Sent message " << sent_message << std::endl;
+    if (totalProcesses < 3){
+        std::cout << "need to have more than 3 proc" << std::endl;
+        return -1;
+    }
+    switch (rank)
+    {
+        case ROOT_PROCCESS:
+            break;
+        case DATA_PROCCESS:
+            break;
+        case RESULT_PROCCESS:
+            break;
+        default: //worker proccess
+            break;
     }
     MPI::Finalize();
     return 0;
