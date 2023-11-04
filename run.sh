84fba2b23b77a47a863c007565858d5aeadb4976
AdomasKairys <adomas.kairys@ktu.edu>
2023-11-04 23:10:16 +0200

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+re='^[0-9]+$'
+if [[ $# != 1 || ($# > 0 && $1 =~ $re)]]; then
+    echo "One argument is req and the argument must be a number"
+fi
 mpic++ main.cpp
-mpirun ./a.out
+mpirun -np $1 ./a.out
